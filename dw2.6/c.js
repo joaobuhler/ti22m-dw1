f6e828db8841e79875c7435073cd30c5964df79b
João Pedro Bühler <joaopedrobuhler26@gmail.com>
2024-09-08 22:24:16 -0300

    att

---
+++ dw2.6/c.js
@@ -0,0 +1,21 @@
+let opcao = 3;
+
+if (opcao === 1) {
+    console.log("Opção 1 selecionada");
+} else if (opcao === 2) {
+    console.log("Opção 2 selecionada");
+} else if (opcao === 3) {
+    console.log("Opção 3 selecionada");
+} else if (opcao === 4) {
+    console.log("Opção 4 selecionada");
+} else if (opcao === 5) {
+    console.log("Opção 5 selecionada");
+} else if (opcao === 6) {
+    console.log("Opção 6 selecionada");
+} else if (opcao === 7) {
+    console.log("Opção 7 selecionada");
+} else if (opcao === 8) {
+    console.log("Opção 8 selecionada");
+} else {
+    console.log("Opção inválida");
+}
---
+++ dw2.6/d.js
@@ -0,0 +1,30 @@
+let opcao = 4;
+
+switch (opcao) {
+    case 1:
+        console.log("Opção 1 selecionada");
+        break;
+    case 2:
+        console.log("Opção 2 selecionada");
+        break;
+    case 3:
+        console.log("Opção 3 selecionada");
+        break;
+    case 4:
+        console.log("Opção 4 selecionada");
+        break;
+    case 5:
+        console.log("Opção 5 selecionada");
+        break;
+    case 6:
+        console.log("Opção 6 selecionada");
+        break;
+    case 7:
+        console.log("Opção 7 selecionada");
+        break;
+    case 8:
+        console.log("Opção 8 selecionada");
+        break;
+    default:
+        console.log("Opção inválida");
+}
