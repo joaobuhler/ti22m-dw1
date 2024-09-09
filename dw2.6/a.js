18c9513979077d8bc2c87e11cfa4153efe6c38b3
João Pedro Bühler <joaopedrobuhler26@gmail.com>
2024-09-08 22:20:20 -0300

    att

---
+++ dw2.6/a.js
@@ -0,0 +1,25 @@
+
+console.log("1");
+
+
+console.log("2");
+
+
+if (condicao1) {
+  
+    if (condicao3) {
+        console.log("A");
+    } else {
+        console.log("B");
+    }
+} else {
+    if (condicao2) {
+        console.log("C");
+    } else {
+        console.log("D");
+    }
+}
+
+console.log("3");
+
+console.log("4");
