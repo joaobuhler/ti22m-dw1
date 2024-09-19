975a8419936f13649032e7c0e1157a568dbaa2de
João Pedro Bühler <joaopedrobuhler26@gmail.com>
2024-09-19 09:34:13 -0300

    att

---
+++ dw2.7/ex1.js
@@ -0,0 +1,11 @@
+function mostrarTabuada(n){
+    let tabuada = 1
+    while (tabuada <= 10){
+        console.log(tabuada * n)
+       
+       tabuada ++ 
+}   
+}
+mostrarTabuada(10)
+
+mostrarTabuada(5)
