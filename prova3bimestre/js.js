39d7e81414d08266c7d2a062714c2361d99a00d5
João Pedro Bühler <joaopedrobuhler26@gmail.com>
2024-10-03 11:57:58 -0300

    att

---
+++ prova3bimestre/js.js
@@ -0,0 +1,18 @@
+
+function somaSerie(cont){
+    let s = []
+    let j = i + 2
+        for (let i = 1; i <= cont ; i++) {
+            
+    s = i * j
+    
+    s++
+            
+        }
+        
+    }
+    
+    console.log(somaSerie(3)) // 1 + 6 + 15 = 22
+    console.log(somaSerie(4)) // 1 + 6 + 15 + 28 = 50
+    
+    
